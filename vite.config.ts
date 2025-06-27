e9d1abefb72fa572c88c9379a5ec10d98abdbea0
Carlos Denner <carlosdenner@gmail.com>
2025-06-27 18:35:16 -0400

    fix: configure base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.VITE_BASE_PATH || '/',
+  base: process.env.VITE_BASE_PATH || "/",
   server: {
     host: "::",
     port: 8080,
